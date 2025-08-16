b81901ab96aa39da6201d64eda03f03c416d5f9a
Mvula88 <mvula88@gmail.com>
2025-08-16 14:05:03 +0200

    Initial commit - clean structure

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+import js from '@eslint/js';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.js', '**/*.mjs'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+        Buffer: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+        exports: 'writable',
+        module: 'writable',
+        require: 'readonly',
+        global: 'readonly',
+      }
+    },
+    rules: {
+      'indent': ['error', 2],
+      'linebreak-style': ['error', 'windows'],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'always'],
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+    }
+  }
+];
